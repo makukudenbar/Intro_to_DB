2c8a5018ea70880271f68c0df5bfa42948643476
Denbar Makuku <makukudenbar1@gmail.com>
2025-12-18 11:32:44 +0200

    Add task_4.sql: Query table metadata using INFORMATION_SCHEMA

---
+++ task_4.sql
@@ -0,0 +1,5 @@
+-- This script prints the full description of the table 'books'
+-- We use the INFORMATION_SCHEMA.COLUMNS table to get metadata
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
