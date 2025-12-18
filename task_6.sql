bd0c97d34f467bafb074cf7860b8a963a2113ee9
Denbar Makuku <makukudenbar1@gmail.com>
2025-12-18 16:00:33 +0200

    Add script to insert multiple customers into database

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,7 @@
--- This script inserts multiple rows into the customer table
+-- Insert multiple rows into the customer table
+
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
+VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
